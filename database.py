244a8de8d950a8ccd44a317462f944089b3c8c1e
rahu278 <rohitkumawat571999@gmail.com>
2026-09-06 10:27:02 +0530

    Add files via upload

--- database.py
+++ database.py
@@ -4,9 +4,9 @@ import mysql.connector
 
 def get_connection():
     return mysql.connector.connect(
-        host=os.getenv("MYSQLHOST", "localhost"),
-        port=int(os.getenv("MYSQLPORT", "3306")),
-        user=os.getenv("MYSQLUSER", "root"),
-        password=os.getenv("MYSQLPASSWORD", "Rahul@8888"),
-        database=os.getenv("MYSQLDATABASE", "finance_db")
+        host=os.environ["MYSQLHOST"],
+        port=int(os.environ.get("MYSQLPORT", "3306")),
+        user=os.environ["MYSQLUSER"],
+        password=os.environ["MYSQLPASSWORD"],
+        database=os.environ["MYSQLDATABASE"]
     )
